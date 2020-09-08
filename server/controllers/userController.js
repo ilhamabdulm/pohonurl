48c026821e5042cd5790e834e0846f2f39d064c2
ilhamabdulm <a.marzmellow21@gmail.com>
2020-09-08 12:18:03 +0700

    change error handler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,6 +26,7 @@ class UserController {
         });
       }
     } catch (err) {
+      console.log(err);
       next(err);
     }
   }
--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -2,13 +2,18 @@ module.exports = (err, _req, res, _next) => {
   let code = 500;
   let errMsg = [];
 
-  if (err.code) {
+  if (err.code && err.name !== 'MongoError') {
     code = err.code;
     errMsg.push(err.message);
   } else {
     if (err.name === 'JsonWebTokenError') {
       code = 401;
       errMsg.push('You are not authorized, missing access token');
+    } else if (err.name === 'MongoError') {
+      if (err.keyPattern.email) {
+        code = 400;
+        errMsg.push('Email sudah terdaftar');
+      }
     } else {
       errMsg.push(err.errors);
     }
