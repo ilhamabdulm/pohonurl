875ad6aad43b47af7b111145068e39627643a998
ilhamabdulm <a.marzmellow21@gmail.com>
2020-09-08 14:37:53 +0700

    change api url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,6 @@
 const API_URL =
-  process.env.NODE_ENV === 'development' ? 'http://192.168.1.114:4000/v1' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://192.168.1.114:4000/v1'
+    : 'https://dry-shore-35382.herokuapp.com/v1';
 
 export default API_URL;
